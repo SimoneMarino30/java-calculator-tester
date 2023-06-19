e8006f93fa9feaeaec6280595f7bfd71cbd86ccd
Simone Marino <simo.marino@outlook.it>
2023-06-19 14:51:37 +0200

    Added public operations methods

--- src/main/java/calculation/Calculator.java
+++ src/main/java/calculation/Calculator.java
@@ -1,5 +1,20 @@
 package calculation;
 
 public class Calculator {
+    // METHODS
+    public float add(float num1, float num2) {
+        return num1 + num2;
+    }
 
+    public float subtract(float num1, float num2) {
+        return num1 - num2;
+    }
+
+    public float divide(float num1, float num2) {
+        return num1 / num2;
+    }
+
+    public float multiply(float num1, float num2) {
+        return num1 * num2;
+    }
 }
